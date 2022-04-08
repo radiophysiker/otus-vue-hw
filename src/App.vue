7304b74303e0873fa5f589c9f1e38f272919f787
radiophysiker <bolshakovdmitry@bk.ru>
2022-04-08 21:52:46 +0300

    add tsx

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
-
-<script lang="ts">
+<script lang="tsx">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
+  render() {
+    return <div>
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Hello world!" />
+    </div>;
   },
 });
 </script>
